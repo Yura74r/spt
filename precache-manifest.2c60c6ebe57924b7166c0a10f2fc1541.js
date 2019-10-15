fcfd0a93b31680d646a9262a236c94567a141302
Yura <yura.rutskih@yandex.ru>
2019-10-15 11:57:10 +0500

    Updates

---
+++ precache-manifest.2c60c6ebe57924b7166c0a10f2fc1541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab61195f16fc45418c868f681dd0985",
+    "url": "/spt/index.html"
+  },
+  {
+    "revision": "b34bad850966e6b184f6",
+    "url": "/spt/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "b34bad850966e6b184f6",
+    "url": "/spt/static/js/2.2579adfd.chunk.js"
+  },
+  {
+    "revision": "5b2d579f1167bbf8f1c9",
+    "url": "/spt/static/js/main.133c53d2.chunk.js"
+  },
+  {
+    "revision": "a3191ab134b513e30db7",
+    "url": "/spt/static/js/runtime-main.9e2e69c8.js"
+  },
+  {
+    "revision": "fc954cee40ba2f884b4efd2dae5647a0",
+    "url": "/spt/static/media/s1.fc954cee.jpg"
+  }
+]);
